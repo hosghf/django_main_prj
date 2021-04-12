a358168caea017a0ff0bc8ee2b8ec74e09ef097d
husein <hosghf@gmail.com>
2021-04-12 14:07:04 -0700

    candle stick added

--- financePanel/urls.py
+++ financePanel/urls.py
@@ -2,12 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.test, name="finance-index"),
+    path('', views.index, name="finance-index"),
     path('index', views.index, name="index"),
-    path('test', views.test, name="test"),
     path('stocks/create', views.stocksCreate, name="stocks.create"),
     path('stocks/<int:pk>/update', views.stocksUpdate, name="stocks.update"),
     path('stocks', views.stocksIndex, name="stocks.update"),
 
-    path('excel-to-db', views.excelToDb, name="excelToDb")
+    path('excel-to-db', views.excelToDb, name="excelToDb"),
+
+    path('candle', views.candle, name="candle"),
+    path('candle2', views.candle2, name="candle2"),
+
+    path('getFinanceData', views.getFinanceData, name="yfinance")
 ]
--- financePanel/views.py
+++ financePanel/views.py
@@ -6,6 +6,7 @@ from .forms import stockModelForm
 from django.core.paginator import Paginator
 import os
 import csv
+import yfinance as yf
 
 # for cvs file
 from django.conf import settings
@@ -15,12 +16,6 @@ from django.core.files.storage import default_storage
 
 def index(request):
     return render(request, 'financePanel/index.html')
-
-def test(request):
-    test2 = stockPrice.objects.get(low='10')
-    # test2 = stockPrice.objects.all()
-    # context = {'test2': test2, 'stock': stock}
-    return render(request, 'financePanel/test.html', {'test2': test2})
 
 def stocksIndex(request):
     stocks = stockPrice.objects.all()
@@ -99,12 +94,94 @@ def excelToDb(request):
                 stok_type=row[3],
                 size=row[4],
                 open=row[5],
-                low=row[6],
-                high=row[7],
-                close_price=row[8],
-                volum=row[9],
+                date=row[6],
+                low=row[7],
+                high=row[8],
+                close_price=row[9],
+                volume=row[10],
                 )
-                
+
     default_storage.delete(path)
     
     return render(request, "financePanel/excel_to_db.html")
+
+
+def getFinanceData(request):
+    if request.method == "POST":
+        symbol = request.POST['symbol']
+        symbolTicker = yf.Ticker(symbol)
+        # history = symbol.history(period='5d')
+        dateFrom = request.POST['dateFrom']
+        dateTo = request.POST['dateTo']
+        history = symbolTicker.history(start=dateFrom, end=dateTo)
+        if not history.index.empty:
+            print('im not empty hahaha')
+
+            for index, row in history.iterrows():
+                created = stockPrice.objects.get_or_create(
+                    date=str(index),
+                    symbol= symbolTicker.info['symbol'],
+                    defaults={
+                        'name': symbolTicker.info['shortName'],
+                        'market': symbolTicker.info['market'],
+                        'high': str(row['High']),
+                        'low': str(row['Low']),
+                        'size': str(row['Volume']),
+                        'open': str(row['Open']),     
+                        'close_price': str(row['Close']),     
+                        'volume': str(row['Volume']),  
+                    },
+                )
+            message = "database is updated"
+            context = { 'message': message }
+            return render(request,'financePanel/yfinance.html', context)
+    
+    return render(request, 'financePanel/yfinance.html')
+
+def candle(request):
+    return render(request, 'financePanel/candle.html')
+
+def candle2(request):
+
+    low = []
+    close = []
+    high = []
+    open = []
+    date = []
+    message = ''
+    found = False
+    post = False
+
+    if request.method == 'POST':
+        symbol = request.POST['symbol']
+        dateFrom = request.POST['dateFrom']
+        dateTo = request.POST['dateTo']
+        stock = False
+
+        if(dateFrom and dateTo):
+            stock = stockPrice.objects.filter(symbol=symbol, date__range=(dateFrom, dateTo))
+
+        if(stock):
+            for rec in stock:
+                low.append(str(rec.low))
+                close.append(str(rec.close_price))
+                high.append(str(rec.high))
+                open.append(str(rec.open))
+                date.append(str(rec.date))
+
+        post = True
+        if stock:
+            found = True
+            message = "your symbol not found in database, you can update database with your symbol"
+
+    context = {
+        'low': low,
+        'close': close,
+        'high': high,
+        'open': open,
+        'date': date,
+        'found': found,
+        'message': message,
+        'post': post
+    }
+    return render(request, 'financePanel/candle2.html', context)
